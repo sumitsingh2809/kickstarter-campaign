18a66c9b5b44aa70fc7310aa0a05df597092ec7b
Sumit Singh <sumitsingh@Rentshers-MacBook-Air.local>
2022-01-05 16:48:26 +0530

    create,list,approve and finalize request

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Card, Grid } from 'semantic-ui-react';
+import { Button, Card, Grid } from 'semantic-ui-react';
 
+import { Link } from '../../routes';
 import web3 from '../../ethereum/web3';
 import Layout from '../../components/Layout';
 import Campaign from '../../ethereum/campaign';
@@ -72,10 +73,27 @@ class CampaignShow extends Component {
             <Layout>
                 <h3>Campaign Show</h3>
                 <Grid>
-                    <Grid.Column width={10}>{this.renderCards()}</Grid.Column>
-                    <Grid.Column width={6}>
-                        <ContributeForm address={this.props.address} />
-                    </Grid.Column>
+                    <Grid.Row>
+                        <Grid.Column width={10}>
+                            {this.renderCards()}
+                        </Grid.Column>
+
+                        <Grid.Column width={6}>
+                            <ContributeForm address={this.props.address} />
+                        </Grid.Column>
+                    </Grid.Row>
+
+                    <Grid.Row>
+                        <Grid.Column>
+                            <Link
+                                route={`/campaigns/${this.props.address}/requests`}
+                            >
+                                <a>
+                                    <Button primary>View Requests</Button>
+                                </a>
+                            </Link>
+                        </Grid.Column>
+                    </Grid.Row>
                 </Grid>
             </Layout>
         );
